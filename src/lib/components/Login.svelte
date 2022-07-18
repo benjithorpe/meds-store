949f177377e647982b0dd9e1c523ffc577bbfcf3
Benjamin Thorpe <benjaminthorpe19@gmail.com>
2022-07-18 20:15:42 +0000

    login and register routes working

--- src/lib/components/Login.svelte
+++ src/lib/components/Login.svelte
@@ -1,17 +1,63 @@
+<script>
+	import { goto } from '$app/navigation';
+
+	let username = '';
+	let password = '';
+	let errorMsg = '';
+
+	const login = async () => {
+		errorMsg = '';
+		const res = await fetch('/api/auth/login', {
+			method: 'POST',
+			body: JSON.stringify({ username, password })
+		});
+		const data = await res.json();
+
+		if (!res.ok) {
+			errorMsg = data.message;
+			// reset form element
+			username = '';
+			password = '';
+			return;
+		}
+
+		goto('/topics');
+	};
+</script>
+
 <div>
-	<form action="" method="POST" on:submit|preventDefault>
+	<form action="" method="POST" on:submit|preventDefault={login}>
 		<fieldset class="flex flex-col gap-5">
-			<legend>Login</legend>
+			<legend class="mb-3 text-3xl text-gray-700">Login</legend>
 			<div>
-				<label for="" class="block">Username</label>
-				<input type="text" class="block py-2 px-4 border" placeholder="input" />
-			</div>
-			<div>
-				<label for="" class="block">Password</label>
-				<input type="text" class="block py-2 px-4 border" placeholder="input" />
+				<label for="" class="block text-gray-500">Username</label>
+				<input
+					type="text"
+					name="username"
+					class="block py-2 px-4 border"
+					placeholder="john"
+					bind:value={username}
+				/>
 			</div>
 
-			<button type="submit" class="bg-gray-500 block">Login</button>
+			<div>
+				<label for="" class="block text-gray-500">Password</label>
+				<input
+					type="password"
+					name="password"
+					class="block py-2 px-4 border"
+					placeholder="password"
+					bind:value={password}
+				/>
+			</div>
+
+			{#if errorMsg}
+				<p class="text-sm text-red-500">{errorMsg}</p>
+			{/if}
+
+			<button type="submit" class="bg-gray-800 rounded-md block w-fit px-4 py-2 text-white">
+				Login
+			</button>
 		</fieldset>
 	</form>
 </div>
--- src/routes/api/auth/login.js
+++ src/routes/api/auth/login.js
@@ -1,1 +1,41 @@
-// Handles the login validation
+import prisma from '../../../lib/prisma.js';
+import * as cookie from 'cookie';
+
+export const POST = async ({ request }) => {
+	const { username, password } = await request.json();
+
+	const user = await prisma.user.findFirst({
+		where: {
+			username: username,
+			AND: {
+				password: password
+			}
+		}
+	});
+
+	if (!user) {
+		return {
+			status: 401,
+			body: {
+				message: 'Invalid email or password.'
+			}
+		};
+	}
+
+	// Set a new cookie if user exists
+	const cookieId = user.id;
+	return {
+		status: 200,
+		headers: {
+			'Set-Cookie': cookie.serialize('session', cookieId, {
+				httpOnly: true,
+				maxAge: 60 * 60 * 24 * 1,
+				sameSite: 'strict',
+				path: '/'
+			})
+		},
+		body: {
+			message: 'Login Successfull...'
+		}
+	};
+};
